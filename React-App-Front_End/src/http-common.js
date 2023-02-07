20a725af847d1af778467a8a4b085097f2777482
Lee Tien Sinh <muzankibut112@gmail.com>
2023-02-07 17:30:33 +0700

    Open comment Authorization

--- React-App-Front_End/src/http-common.js
+++ React-App-Front_End/src/http-common.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
-        // "Authorization": `Bearer ${cookies}`
+        "Authorization": `Bearer ${cookies}`
     }
 });
 
